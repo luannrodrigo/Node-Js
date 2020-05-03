ef127c9de20ed85fcbeb5d2d95bc34db606efd1a
jessica <jessica.carolina26@gmail.com>
2020-05-03 10:12:29 -0300

    Configurando estrutura

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,9 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'hello luann' }),
-);
+routes.get('/', (request, response) => {
+  const { name, email } = request.body;
+  return response.json({ message: 'hello luann' });
+});
 
 export default routes;

6adc5e063a515dbf79ba3ea57394399e23f0eddb
jessica <jessica.carolina26@gmail.com>
2020-05-03 13:34:10 -0300

    criacao de models

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,32 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+class AppointmentRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointments = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return findAppointments || null;
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentRepository;
